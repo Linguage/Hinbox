6b0a1d57e0b2006373ee0fe711b47cfbf1e605ec
Linguage <peakhan618@outlook.com>
2025-12-01 22:40:31 +0800

    rss-fix

--- app/rss.xml/route.ts
+++ app/rss.xml/route.ts
@@ -1,5 +1,7 @@
 import { getEmailsFromPosts } from '@/lib/posts';
 
+export const dynamic = 'force-static';
+
 function escapeXml(value: string): string {
   return value
     .replace(/&/g, '&amp;')
